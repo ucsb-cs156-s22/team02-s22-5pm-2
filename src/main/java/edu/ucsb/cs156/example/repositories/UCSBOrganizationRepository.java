213006aef26576a6d3bd95606fb86f078b8e6c0f
Hyun Kyum Kim <hyunkyumkim@ucsb.edu>
2022-04-27 16:41:29 -0700

    hkk - added @Repository for UCSBOrganization Repository

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
@@ -6,6 +6,7 @@ import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UCSBOrganizationRepository extends CrudRepository<UCSBOrganization, String>{
     
 }
