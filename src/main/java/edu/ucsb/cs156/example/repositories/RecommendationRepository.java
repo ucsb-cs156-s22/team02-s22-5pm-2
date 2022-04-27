0068b01590dba9e782a5f7c44de6f648e1288fe8
Limin Ding <liminding@ucsb.edu>
2022-04-27 00:58:50 -0700

    ld -Create database table for Recommendation

--- src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RecommendationRepository extends CrudRepository<Recommendation, Long> {
+public interface RecommendationRepository extends CrudRepository<Recommendation, Long> {
     
 }
