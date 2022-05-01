5e48e77820c35f577ebe740c797b1fbf700bb6a1
ryanlavu <ryanvu@ucsb.edu>
2022-05-01 16:42:51 -0700

    rv - fixes to Controller added

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticleController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticleController.java
@@ -59,17 +59,18 @@ public class ArticleController extends ApiController {
     @PostMapping("/post")
     public Article postArticle(
 
-            @ApiParam("title") @RequestParam String title,
-            @ApiParam("url") @RequestParam String url,
-            @ApiParam("explanation") @RequestParam String explanation,
+
             @ApiParam("email") @RequestParam String email,
-            @ApiParam("date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("localDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime localDateTime)
+            @ApiParam("explanation") @RequestParam String explanation,
+            @ApiParam("date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("dateAdded") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateAdded,
+            @ApiParam("title") @RequestParam String title,
+            @ApiParam("url") @RequestParam String url)
             throws JsonProcessingException {
 
         // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
         // See: https://www.baeldung.com/spring-date-parameters
 
-        log.info("localDateTime={}", localDateTime);
+        log.info("dateAdded={}", dateAdded);
 
         Article article = new Article();
         article.setTitle(title);
