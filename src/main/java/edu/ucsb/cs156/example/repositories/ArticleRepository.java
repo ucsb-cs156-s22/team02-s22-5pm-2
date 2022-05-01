3a6e18263cc3cf6a786c6554d015581ec0ae0e9b
ryanlavu <ryanvu@ucsb.edu>
2022-05-01 16:20:59 -0700

    rv - Controller added

--- src/main/java/edu/ucsb/cs156/example/repositories/ArticleRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/ArticleRepository.java
@@ -1,12 +1,12 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.UCSBDate;
+import edu.ucsb.cs156.example.entities.Article;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ArticleRepository extends CrudRepository<UCSBDate, Long> {
+public interface ArticleRepository extends CrudRepository<Article, Long> {
 
 }
